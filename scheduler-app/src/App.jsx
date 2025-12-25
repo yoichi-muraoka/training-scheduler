2a01ae97ff10580694de5e663986d7c5d39c6329
Muraoka <muraoka@villhill.net>
2025-12-25 13:13:48 +0900

    ChakraUIの導入

--- scheduler-app/src/App.jsx
+++ scheduler-app/src/App.jsx
@@ -1,22 +1,25 @@
 import { Routes, Route, Link } from "react-router-dom";
 import CoursesPage from "./pages/CoursesPage";
+import { Heading, Container } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <h1>研修スケジュール管理システム</h1>
-      <header style={{ padding: 16 }}>
+    <Container py={5}>
+      <Heading as="h1" size="2xl" mb={4}>
+        研修スケジュール管理システム
+      </Heading>
+      <header>
         <nav>
           <Link to="/">Home</Link> | <Link to="/courses">Courses</Link>
         </nav>
       </header>
-      <main style={{ padding: 16 }}>
+      <main>
         <Routes>
           <Route path="/" element={<div>Welcome to Scheduler App</div>} />
           <Route path="/courses" element={<CoursesPage />} />
         </Routes>
       </main>
-    </div>
+    </Container>
   );
 }
 
--- scheduler-app/src/main.jsx
+++ scheduler-app/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChakraProvider value={defaultSystem}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
     </QueryClientProvider>
   </StrictMode>
 );
